df89662bc7f72dccbab730810875f45c37e09f31
MalcyL <malcolm@landonsonline.me.uk>
2014-12-09 12:35:07 +0000

    TENT-752 Make provider constants match pkgcloud.

--- src/Manifesto/common.inc.php
+++ src/Manifesto/common.inc.php
@@ -6,5 +6,5 @@ define('FORMAT_ZIP', 'zip');
 define('FORMAT_TARGZ', 'targz');
 define('FORMAT_TARBZ', 'tarbz');
 
-define('FILE_TYPE_S3', 's3');
-define('FILE_TYPE_CF', 'cloudfiles');
+define('FILE_TYPE_S3', 'amazon');
+define('FILE_TYPE_CF', 'rackspace');
